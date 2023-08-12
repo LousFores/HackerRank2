a53faf5ee8ea65c0274780b58727a9584f26d25b
Louis Fores <114371230+LousFores@users.noreply.github.com>
2023-08-12 11:18:39 +0700

    Add files via upload

---
+++ DiemChuyenTiep.java
@@ -0,0 +1,42 @@
+import java.awt.geom.AffineTransform;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DiemChuyenTiep {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Size number: ");
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print("Number is " + i + ":");
+            int number = scanner.nextInt();
+            list.add(number);
+        }
+
+        int count = 0;
+        int count1 = 0;
+        int count0 = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == 0) count1++;
+            if (list.get(i) == 1) count0++;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == 0) count++;
+            if (list.get(i) == 1) break;
+        }
+        System.out.println("Result return: " + solve(list));
+    }
+    public static int solve(List<Integer> arr) {
+        int count = -1;
+        for (int i = 0; i < arr.size()-1; i++) {
+            if ((arr.get(i) == 0) && (arr.get(i+2)==1)) {
+                count = i + 2;
+                break;
+            }
+        }
+        return count;
+    }
+
+}
